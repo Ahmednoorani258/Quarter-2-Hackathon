00870792ddb6dd1999c40fec670e2e42b569b88e
Muhammad ahmed noorani noorani <159555834+Ahmednoorani258@users.noreply.github.com>
2024-10-24 11:23:04 +0000

    name update

---
+++ 01 hackathon_minus_one/milestone-3DynamicResumeBuilder/script.ts
@@ -0,0 +1,21 @@
+let myName= document.getElementById('name')
+let designation= document.getElementById('designation')
+let phone = document.getElementById('phone')
+let email = document.getElementById('email')
+let address = document.getElementById('address')
+let passingyear = document.getElementById('passingyear')
+let degree = document.getElementById('degree')
+let institution = document.getElementById('institution')
+let skills = document.getElementById('skills')
+let language = document.getElementById('language')
+let startyear = document.getElementById('startyear')
+let endyear = document.getElementById('endyear')
+let company = document.getElementById('company')
+let companylocation = document.getElementById('companylocation')
+let jobtitle = document.getElementById('jobtitle')
+let responsibilities = document.getElementById('responsibilities')
+
+let submitbtn = document.getElementById('submitbtn')
+let form = document.getElementById('form')
+
+
