72386aa800119018cc47a7ae9b9235dd96837498
muhammad ahmed noorani <ahmednoorani258@gmail.com>
2024-10-28 11:21:17 -0700

    milestone 1 update without responsive

--- 01 hackathon_minus_one/milestone-3DynamicResumeBuilder/script.ts
+++ 01 hackathon_minus_one/milestone-3DynamicResumeBuilder/script.ts
@@ -18,6 +18,8 @@ let responsibilities:any = document.getElementById('responsibilities')
 let submitbtn :any= document.getElementById('submitbtn')
 let form:any = document.getElementById('form')
 
+console.log(myName,designation,phone,email,address,passingyear,degree,institution,skills,language,startyear,endyear,company,companylocation,jobtitle,responsibilities,submitbtn,form)
+
 form?.addEventListener('submit',(e) => {
     e.preventDefault()
     localStorage.setItem("myname",myName.value)
@@ -36,5 +38,7 @@ form?.addEventListener('submit',(e) => {
     localStorage.setItem("companylocation",companylocation.value)
     localStorage.setItem("jobtitle",jobtitle.value)
     localStorage.setItem("responsibilities",responsibilities.value)
+
+    window.location.href
 })
 
